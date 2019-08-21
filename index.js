df3bf3bda11e5695a5cc8a9697c920cfa9f10eca
guseyn <guseynism@gmail.com>
2019-08-21 20:50:23 +0400

    fix collision

--- index.js
+++ index.js
@@ -1,12 +1,5 @@
 let cutieHttps = require('@cuties/http');
 
-delete cutieHttps.CreatedAgent;
-delete cutieHttps.CreatedHttpServer;
-delete cutieHttps.HttpGetRequest;
-delete cutieHttps.HttpRequest;
-delete cutieHttps.ResponseFromHttpGetRequest;
-delete cutieHttps.ResponseFromHttpRequest;
-
 cutieHttps.CreatedAgent = require('./src/agent/CreatedAgent');
 cutieHttps.CreatedAgentConnection = require('./src/agent/CreatedAgentConnection');
 cutieHttps.CreatedHttpsServer = require('./src/https/CreatedHttpsServer');
